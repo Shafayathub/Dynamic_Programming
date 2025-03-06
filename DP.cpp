b05277757ff6b3ea9c9b9675fdc90b0ce4c77c1f
Shafayathub <shafayat.ms@gmail.com>
2025-03-07 03:39:15 +0600

    example added

--- DP.cpp
+++ DP.cpp
@@ -2,14 +2,17 @@
 using namespace std;
 
 int fibo(int n){
+    if(n==0){
+        return 0;
+    }
     if(n==1){
         return 1;
     }
-    return n*fibo(n-1);
+    return fibo(n-1) + fibo(n-2);
 }
 
 int main() {
-    cout << fibo(5);
+    cout << fibo(8);
     
     return 0;
-}
+} 
